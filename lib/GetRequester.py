be01d536251594a530c03ec1aec48c4d8995a7ea
Nikapwente <hrsmajor@gmail.com>
2024-01-28 18:08:21 +0300

    Assignment Complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,14 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
         pass
+        response = json.loads(self.get_response_body())
+        return response
+    
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
